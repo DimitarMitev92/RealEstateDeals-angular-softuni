0616ad03e8619bfa8b405cdbcd97c9cdfc6900c6
Dimitar Mitev <d.mitev920123@gmail.com>
2023-06-25 00:44:19 +0300

    About us section

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeroSectionComponent],
+  declarations: [AppComponent, HeaderComponent, HeroSectionComponent, AboutUsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
