2c1b86de692fffd237ecbc238c42b7be2d119fb8
Dimitar Mitev <d.mitev920123@gmail.com>
2023-06-27 23:23:37 +0300

    Login and Register templates

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,10 +7,23 @@ import { HeaderComponent } from './components/header/header.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeroSectionComponent, AboutUsComponent, CatalogComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HeroSectionComponent,
+    AboutUsComponent,
+    CatalogComponent,
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    ContactUsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
