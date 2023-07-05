99dacaeb2adbe63a91dac88b3acdda584dc05c9e
Dimitar Mitev <d.mitev920123@gmail.com>
2023-07-05 09:55:23 +0300

    Creating Not Found Page

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [SpinnerComponent],
+  declarations: [SpinnerComponent, NotFoundComponent],
   imports: [CommonModule],
-  exports: [SpinnerComponent],
+  exports: [SpinnerComponent, NotFoundComponent],
 })
 export class SharedModule {}
