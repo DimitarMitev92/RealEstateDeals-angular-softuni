db25c937d91ae07f69ab66cf68e63ed97768ea9f
Dimitar Mitev <d.mitev920123@gmail.com>
2023-07-05 10:36:25 +0300

    Profile page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { CoreModule } from './core/core.module';
 import { MainModule } from './main/main.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroSectionComponent } from './main/hero-section/hero-section.component';
+import { CatalogComponent } from './main/catalog/catalog.component';
+import { AboutUsComponent } from './main/about-us/about-us.component';
+import { ContactUsComponent } from './main/contact-us/contact-us.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DetailsComponent } from './main/details/details.component';
+import { CreateComponent } from './user/create/create.component';
+import { EditComponent } from './user/edit/edit.component';
+import { ProfileComponent } from './user/profile/profile.component';
+
+const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', component: HeroSectionComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: ':id/details', component: DetailsComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'edit', component: EditComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CoreModule,
     MainModule,
     SharedModule,
     AuthModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [CreateComponent, EditComponent],
+  declarations: [CreateComponent, EditComponent, ProfileComponent],
   imports: [CommonModule],
-  exports: [CreateComponent, EditComponent],
+  exports: [CreateComponent, EditComponent, ProfileComponent],
 })
 export class UserModule {}
