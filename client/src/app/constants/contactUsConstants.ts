4c65e3046f1925105659c2e348e1e70a78701985
Dimitar Mitev <d.mitev920123@gmail.com>
2023-07-10 13:36:46 +0300

    Create contact us constants

---
+++ client/src/app/constants/contactUsConstants.ts
@@ -0,0 +1,11 @@
+export const contactUsConstants = {
+  heading: 'Contact Us',
+  fullName: 'Full Name',
+  fullNamePlaceholder: 'Dimitar Mitev',
+  email: 'Email',
+  emailPlaceholder: 'd.mitev@gmail.com',
+  phoneNumber: 'Phone Number',
+  phoneNumberPlaceholder: '+123 12 123 123',
+  message: 'Your Message',
+  btnText: 'Send',
+};
--- client/src/app/core/contact-us/contact-us.component.ts
+++ client/src/app/core/contact-us/contact-us.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { contactUsConstants } from 'src/app/constants/contactUsConstants';
+
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent {
-
+  contactUsConstants = contactUsConstants;
 }
