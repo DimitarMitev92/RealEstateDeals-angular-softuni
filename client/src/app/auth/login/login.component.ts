ed214736e02b86759cc0c8c724d3fe8ac5ab7279
Dimitar Mitev <d.mitev920123@gmail.com>
2023-07-10 13:12:10 +0300

    Create login constants

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { loginConstants } from 'src/app/constants/loginConstants';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  loginConstant = loginConstants;
 }
---
+++ client/src/app/constants/loginConstants.ts
@@ -0,0 +1,10 @@
+export const loginConstants = {
+  heading: 'Login',
+  email: 'Email',
+  emailPlaceholder: 'd.mitev@gmail.com',
+  password: 'Password',
+  passwordPlaceholder: '******',
+  btnText: 'Login',
+  account: "Don't have an account?",
+  btnAccount: 'Register here',
+};
