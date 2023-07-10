2b2f710b852e10c75112ae490d37d174cb69f3f1
Dimitar Mitev <d.mitev920123@gmail.com>
2023-07-10 13:05:09 +0300

    create form constants

---
+++ client/src/app/constants/createConstants.ts
@@ -0,0 +1,13 @@
+export const createConstants = {
+  heading: 'Create an offer',
+  title: 'Title',
+  titlePlaceholder: 'Luxury villa with pool',
+  location: 'Location',
+  locationPlaceholder: 'USA, New York 1234 AU',
+  image: 'Image Link',
+  imagePlaceholder: 'https://...',
+  price: 'Price',
+  pricePlaceholder: '1000000.00',
+  information: 'Information about the offer',
+  btnText: 'Create',
+};
--- client/src/app/user/create/create.component.ts
+++ client/src/app/user/create/create.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { createConstants } from 'src/app/constants/createConstants';
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent {
-
+  createConstants = createConstants;
 }
