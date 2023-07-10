aca5a0258a1190cea6ac6b7923ddc443b4a60db3
Dimitar Mitev <d.mitev920123@gmail.com>
2023-07-10 13:51:18 +0300

    Create profile constants

---
+++ client/src/app/constants/profileConstants.ts
@@ -0,0 +1,6 @@
+export const profileConstants = {
+  heading: 'My profile',
+  fullName: 'Full name:',
+  email: 'Email:',
+  offers: 'Your offers',
+};
--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { profileConstants } from 'src/app/constants/profileConstants';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
-
+  profileConstants = profileConstants;
 }
