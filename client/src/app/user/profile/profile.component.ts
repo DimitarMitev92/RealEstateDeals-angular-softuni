a36f37167fb20fc579553da58beb94405c33aeaf
Dimitar Mitev <d.mitev920123@gmail.com>
2023-07-17 11:28:05 +0300

    Profile page functionality

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { profileConstants } from 'src/app/constants/profileConstants';
+import { UserCRUDService } from '../user-crud.service';
+import { IOfferReturnData } from 'src/app/interfaces/offerInterfaces';
+import { IRegisterData } from 'src/app/interfaces/authInterfaces';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   profileConstants = profileConstants;
+
+  userData!: IRegisterData;
+  userOffers!: IOfferReturnData[];
+
+  constructor(private userCRUD: UserCRUDService) {}
+
+  ngOnInit(): void {
+    let userDataJSON = localStorage.getItem('userData');
+    if (userDataJSON !== null) {
+      this.userData = JSON.parse(userDataJSON);
+      console.log(this.userData);
+      let userID = JSON.parse(userDataJSON)._id;
+      this.userCRUD.getAllOffers().subscribe({
+        next: (response) => {
+          this.userOffers = response.filter(
+            (offer) => offer._ownerId === userID
+          );
+          console.log(this.userOffers);
+        },
+        error: (msg) => {
+          console.log(msg);
+        },
+      });
+    } else {
+      console.log('userData is not found in localStorage.');
+    }
+  }
 }
